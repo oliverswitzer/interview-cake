7faf6be0987dfcb20f59a552b8230403b6cb039a
Oliver Switzer <oliverswitzer@gmail.com>
2020-06-03 14:04:57 -0400

    Add problem explanation to palindrome problem

--- hashmaps/permutation-palindrome/has_palindrome_permutation.rb
+++ hashmaps/permutation-palindrome/has_palindrome_permutation.rb
@@ -1,3 +1,17 @@
+# Write an efficient method that checks whether any permutation ↴ of an input string is a palindrome. ↴
+#
+# You can assume the input string only contains lowercase letters.
+#
+# Examples:
+#
+# "civic" should return true
+# "ivicc" should return true
+# "civil" should return false
+# "livci" should return false
+# "But 'ivicc' isn't a palindrome!"
+#
+# If you had this thought, read the question again carefully. We're asking if any permutation of the string is a palindrome. Spend some extra time ensuring you fully understand the question before starting. Jumping in with a flawed understanding of the problem doesn't look good in an interview.
+
 def has_palindrome_permutation?(the_string)
   char_occurences = {}
 
