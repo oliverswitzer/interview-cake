94ec4338445460d7759163a6400bdcbba361503f
Oliver Switzer <oliverswitzer@gmail.com>
2020-07-08 10:35:45 -0400

    Implement find_repeat with O(logN) time and O(1) space by sorting numbers in place first

--- sorting-searching-and-logarithms/find-duplicate-optimize-for-space/find_repeat.rb
+++ sorting-searching-and-logarithms/find-duplicate-optimize-for-space/find_repeat.rb
@@ -1,17 +1,30 @@
+# Find a duplicate, Space Edition™.
+#
+# We have an array of integers, where:
+#
+# The integers are in the range 1..n
+# The array has a length of n+1
+# It follows that our array has at least one integer which appears at least twice. But it may have several duplicates, and each duplicate may appear more than twice.
+#
+# Write a method which finds an integer that appears more than once in our array. (If there are multiple duplicates, you only need to find one of them.)
+#
+# We're going to run this method on our new, super-hip MacBook Pro With Retina Display™. Thing is, the damn thing came with the RAM soldered right to the motherboard, so we can't upgrade our RAM. So we need to optimize for space!
+
 def find_repeat(numbers)
-  repeats = {}
-  numbers.each do |num|
-    if repeats[num]
-      return num
+  numbers = numbers.sort
+  last_num = numbers.first
+
+  (1...numbers.length).each do |i|
+    if numbers[i] == last_num
+      return numbers[i]
     else
-      repeats[num] = 0
+      last_num = numbers[i]
     end
   end
 
-  0
+  # Whoops--no duplicate.
+  raise 'No duplicate!'
 end
-
-
 
 
 
